b6a5cc89c828e1626bf8d18d85f3c757b4722cb9
Temesgen Sida <temesgensids@gmail.com>
2026-09-05 23:26:10 +0300

    feat(layout): apply global stylesheet and application head metadata

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>LakiDrop • WebRTC Peer-to-Peer File Transfer</title>
+</svelte:head>
+
+{@render children()}
